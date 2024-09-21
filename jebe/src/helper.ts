6e0e517f811326032d2b9c519a36080d53b686c1
PAVOL SLOVAK <pavol.slovak1995@gmail.com>
2024-09-21 14:59:12 +1000

    update

--- jebe/src/helper.ts
+++
@@ -1,3 +0,0 @@
-export const hey = () => {
-  console.log("Hey there!");
-};
--- jebe/src/middlewares/cors.ts
+++
@@ -1,9 +0,0 @@
-import cors from "cors";
-
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-export const corsMiddleware = cors(corsOptions);
