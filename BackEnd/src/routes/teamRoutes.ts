36b627b6d9d334ef33966767797ffeb546e9991f
PAVOL SLOVAK <pavol.slovak1995@gmail.com>
2024-09-21 14:57:43 +1000

    npm run dev

--- BackEnd/src/routes/teamRoutes.ts
+++ BackEnd/src/routes/teamRoutes.ts
@@ -1,3 +1,4 @@
+console.log("Importing teamRoutes.ts");
 import express from "express";
 import { authenticateToken } from "../middlewares/authenticateToken.js";
 import { getTeams } from "../controllers/teamController.js";
@@ -6,3 +7,6 @@ export const router = express.Router();
 
 // API routes for teams
 router.get("/:teamId", authenticateToken, getTeams);
+router.get("/", (req, res) => {
+  res.send("GET request to the homepage");
+});
